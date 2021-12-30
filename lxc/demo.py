c12eb7cc6ad85eecc417803f497c88f4fb2ad636
that-github-guy <96881741+that-github-guy@users.noreply.github.com>
2021-12-30 17:43:41 -0500

    Change naming scheme again

--- lxc/demo.py
+++ lxc/demo.py
@@ -1,8 +1,8 @@
-import lxc_main_functions
+import lxc_functions
 import sys
 
-print(lxc_main_functions.create_instance(name=sys.argv[1], os=['ubuntu','focal','amd64']))
-print(lxc_main_functions.setup_instance(name=sys.argv[1], commands=[['apt', 'install', '-y', 'openssh-server'], ['adduser', 'newuser'], ['usermod', '-aG', 'sudo', 'newuser']])) 
-print(lxc_main_functions.get_ip_instance(name=sys.argv[1])[0])
+print(lxc_functions.create_instance(name=sys.argv[1], os=['ubuntu','focal','amd64']))
+print(lxc_functions.setup_instance(name=sys.argv[1], commands=[['apt', 'install', '-y', 'openssh-server'], ['adduser', 'newuser'], ['usermod', '-aG', 'sudo', 'newuser']])) 
+print(lxc_functions.get_ip_instance(name=sys.argv[1])[0])
 
 # ['echo', '-e', f'rootpass\nrootpass', '|', 'passwd', 'newuser'] for non-interactive use

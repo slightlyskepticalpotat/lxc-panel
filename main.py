a91f9c761826d4d800a09bf58b2174f65181a730
that-github-guy <96881741+that-github-guy@users.noreply.github.com>
2021-12-30 15:22:54 -0500

    Remove security issues, make interactive

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import lxc_functions
 import sys
 
 print(lxc_functions.create_instance(name=sys.argv[1], os=['ubuntu','focal','amd64']))
-print(lxc_functions.setup_instance(name=sys.argv[1], commands=[['apt', 'install', '-y', 'openssh-server'], ['adduser', 'newuser'], ['usermod', '-aG', 'sudo', 'newuser'], ['echo', '-e', f'rootpass\nrootpass', '|', 'passwd', 'newuser']])) # VERY INSECURE, should change
-                                                                                                            
+print(lxc_functions.setup_instance(name=sys.argv[1], commands=[['apt', 'install', '-y', 'openssh-server'], ['adduser', 'newuser'], ['usermod', '-aG', 'sudo', 'newuser']])) 
+
+# ['echo', '-e', f'rootpass\nrootpass', '|', 'passwd', 'newuser'] for non-interactive use

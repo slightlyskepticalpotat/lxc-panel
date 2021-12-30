c15e5e04c578fed7e501edbbc07d07a244cc5861
that-github-guy <96881741+that-github-guy@users.noreply.github.com>
2021-12-30 15:18:36 -0500

    Add example script

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-import flask_hook # for what? only god knows
+import lxc_functions
+import sys
+
+print(lxc_functions.create_instance(name=sys.argv[1], os=['ubuntu','focal','amd64']))
+print(lxc_functions.setup_instance(name=sys.argv[1], commands=[['apt', 'install', '-y', 'openssh-server'], ['adduser', 'root'], ['usermod', '-aG', 'sudo', 'root'], ['echo', '-e', f'rootpass\nrootpass', '|', 'passwd', 'root']])) # VERY INSECURE, should change
+                                                                                                            

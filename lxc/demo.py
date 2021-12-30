255b3b017b369b3b39753d05c31aa0c8b10b2f32
that-github-guy <96881741+that-github-guy@users.noreply.github.com>
2021-12-30 16:57:39 -0500

    Print internal ip in demo

--- lxc/demo.py
+++ lxc/demo.py
@@ -3,5 +3,6 @@ import sys
 
 print(lxc_main_functions.create_instance(name=sys.argv[1], os=['ubuntu','focal','amd64']))
 print(lxc_main_functions.setup_instance(name=sys.argv[1], commands=[['apt', 'install', '-y', 'openssh-server'], ['adduser', 'newuser'], ['usermod', '-aG', 'sudo', 'newuser']])) 
+print(lxc_main_functions.get_ip_instance(name=sys.argv[1])[0])
 
 # ['echo', '-e', f'rootpass\nrootpass', '|', 'passwd', 'newuser'] for non-interactive use
